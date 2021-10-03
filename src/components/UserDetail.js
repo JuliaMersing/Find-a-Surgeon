86cc5d2c1df8309dd1de4c5220b44982e9c4e875
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-10-03 17:30:36 +0200

    add data picker component

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,41 +1,34 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import Booking from "./Booking";
 
 const UserDetail = (props) => {
-  const Example = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    return (
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-      />
-    );
-  };
-
   return (
-    <div className="card">
-      <Link to="/">Home</Link>
-      <header>
-        <h2>{props.user.name}</h2>
-      </header>
-      <section>
-        <img
-          className="card__img"
-          src={props.user.image}
-          alt={props.user.name}
-        />
-        <h4 className="card__title">{props.user.name}</h4>
-        <ul className="ml-1 mt-1">
-          <li>Gender: {props.user.gender}</li>
-          <li>Email: {props.user.email}</li>
-          <li>City: {props.user.city}</li>
-          <li>Country: {props.user.country}</li>
-        </ul>
-        <Example />
-      </section>
-    </div>
+    <>
+      <div className="card">
+        <Link to="/">Home</Link>
+        <header>
+          <h2>{props.user.name}</h2>
+        </header>
+        <section>
+          <img
+            className="card__img"
+            src={props.user.image}
+            alt={props.user.name}
+          />
+          <h4 className="card__title">{props.user.name}</h4>
+          <ul className="ml-1 mt-1">
+            <li>Gender: {props.user.gender}</li>
+            <li>Email: {props.user.email}</li>
+            <li>City: {props.user.city}</li>
+            <li>Country: {props.user.country}</li>
+          </ul>
+        </section>
+      </div>
+      <div>
+        <Booking />
+      </div>
+    </>
   );
 };
 

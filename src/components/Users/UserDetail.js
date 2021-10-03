b8919ac1ba54bc5639ab08e4827c7bad1fa4840f
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-10-03 19:37:06 +0200

    change header

--- src/components/Users/UserDetail.js
+++ src/components/Users/UserDetail.js
@@ -6,8 +6,8 @@ import Registration from "../Registration";
 const UserDetail = (props) => {
   return (
     <>
+      <Link to="/">Home</Link>
       <div className="card">
-        <Link to="/">Home</Link>
         <header>
           <h2>{props.user.name}</h2>
         </header>
@@ -18,21 +18,21 @@ const UserDetail = (props) => {
             alt={props.user.name}
           />
           <h4 className="card__title">{props.user.name}</h4>
-          <ul className="ml-1 mt-1">
-            <li>
-              About: Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Ad dolor maxime quas praesentium, impedit hic reprehenderit
-              quibusdam voluptatum dicta eaque, exercitationem ipsam. Nobis
-              beatae nisi officiis? Quisquam pariatur ratione enim.
-            </li>
-            <li>Gender: {props.user.gender}</li>
-            <li>Email: {props.user.email}</li>
-            <li>City: {props.user.city}</li>
-            <li>Country: {props.user.country}</li>
-          </ul>
         </section>
       </div>
       <div>
+        <ul className="ml-1 mt-1">
+          <li>
+            About: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad
+            dolor maxime quas praesentium, impedit hic reprehenderit quibusdam
+            voluptatum dicta eaque, exercitationem ipsam. Nobis beatae nisi
+            officiis? Quisquam pariatur ratione enim.
+          </li>
+          <li>Gender: {props.user.gender}</li>
+          <li>Email: {props.user.email}</li>
+          <li>City: {props.user.city}</li>
+          <li>Country: {props.user.country}</li>
+        </ul>
         <Registration />
         <Booking />
       </div>

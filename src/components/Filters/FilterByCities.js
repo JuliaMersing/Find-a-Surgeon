c498043d3bb58f2eeb754fa56c1494801b443314
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-10-19 12:03:09 +0200

    add new style

--- src/components/Filters/FilterByCities.js
+++ src/components/Filters/FilterByCities.js
@@ -9,68 +9,6 @@ const FilterByCities = (props) => {
     });
   };
 
-  /*  
- FILTRO COMO FILTER BY NAME
-
- return (
-    <>
-      <label className="form__label" htmlFor="city">
-        City:
-      </label>
-      <input
-        className="form__input"
-        type="text"
-        name="city"
-        id="city"
-        value={props.FilterByCities}
-        onChange={handleChange}
-      />
-    </>
-  );
-
-FILTRO COMO GENDER
-
- return (
-    <>
-      <label className="form__label" htmlFor="city">
-        City:
-      </label>
-      <select
-        className="form__input"
-        name="city"
-        id="city"
-        value={props.FilterByCities}
-        onChange={handleChange}
-      >
-        <option value="">All</option>
-        <option value="Lille">Lille</option>
-        <option value="Vikersund">Vikersund</option>
-        <option value="Vitry-sur-Seine">Vitry-sur-Seine</option>
-      </select>
-    </>
-
-}; */
-
-  /*const renderCities = () => {
-    return props.cities.map((city, index) => {
-      return (
-        <li key={index}>
-          <label className="form__label">
-            <input
-              className="form__inputCity"
-              type="checkbox"
-              name="city"
-              value={city}
-              checked={props.filterCities.includes(city)}
-              onChange={handleChange}
-            />
-            {city}
-          </label>
-        </li>
-      );
-    });
-  };*/
-
   const renderCities = () => {
     return props.cities.map((city, index) => {
       return (
@@ -99,15 +37,6 @@ FILTRO COMO GENDER
       </select>
     </>
   );
-
-  /*return (
-    <>
-      <label className="form__label display-block" htmlFor="city">
-        City:
-      </label>
-      <ul className="filterCities">{renderCities()}</ul>
-    </>
-  );*/
 };
 
 export default FilterByCities;
